9256b87da9d5e0088cd07f5f446894e35da4150c
Kazuki74 <allykakki@me.com>
2018-03-21 16:02:45 +0000

    finished 10.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -19,7 +22,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:sucsess] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
 
 private
 
@@ -27,3 +43,16 @@ private
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
+  def logged_in_user
+    unless logged_in?
+    store_location
+    flash[:danger] = "Please Log in."
+    redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find_by(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+end
