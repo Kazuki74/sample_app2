d8aa6db92124b6f76ddc7ea3b2291e6fe887b9d2
Kazuki74 <allykakki@me.com>
2018-03-31 04:44:11 +0000

    finished 13.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   
@@ -54,13 +55,7 @@ private
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
   end
   
-  def logged_in_user
-    unless logged_in?
-    store_location
-    flash[:danger] = "Please Log in."
-    redirect_to login_url
-    end
-  end
+
   
   def correct_user
     @user = User.find_by(params[:id])
